afb69bbe77159aa51999d94597ec249d75b0f57e
Micael Robles <micael.robles@gmail.com>
2020-06-30 15:03:26 -0300

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./styles.css";
 import Board from "./Board";
 import Input from "./Input";
